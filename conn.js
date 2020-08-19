bfa7861dcf8eb97cbd198950dff78a46d0ff97eb
Mufaris4 <37739496+Mufaris4@users.noreply.github.com>
2020-08-19 07:20:33 +0530

    conn.js

---
+++ conn.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+const { DataTypes } = Sequelize;
+
+
+const app = express();
+const port = 4004;
+var con = new Sequelize('asd', 'root', '', {
+        dialect: 'mysql'
+});
+
+
+const user = con.define('user',{
+	uuid:{
+		type: DataTypes.UUID,
+		primaryKey: true,
+		
+	},
+	name: DataTypes.Text,
+	bio: DataTypes.Text
+}) ;
+
+
+con.sync({
+	logging: console.log,
+	force: true
+})
+.then(() =>{
+	console.log('Connection sccess');
+})
+.catch(err => {
+	console.error('unable to connect',err)
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
